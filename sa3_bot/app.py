c33594ae21070d7a0ad349898be58f06a5553bef
Seungjae Park <astro.psj@gmail.com>
2024-05-05 00:34:55 +0900

    Update app.py

--- sa3_bot/app.py
+++ sa3_bot/app.py
@@ -77,7 +77,7 @@ def find_company_by_business(query_text):
     answers = []
     for asset_id, business_detail in find_similar_business_companies(business_embedding):
         if asset_id not in checked_ids:
-            print(business_detail.split('\n')[0])
+            print(business_detail.split("\n")[0])
             answer, reason = is_relevant_business(business_text, business_detail)
             if answer:
                 answers.append((asset_id, reason))
@@ -85,8 +85,12 @@ def find_company_by_business(query_text):
     return (business_text, answers)
 
 
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("주식 스크리닝 봇입니다. 관리자(@psj1026)가 서버를 켰을 때만 동작합니다.")
+
+
 async def search(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    query_text = update.message.text.replace('/search', '').strip()
+    query_text = update.message.text.replace("/search", "").strip()
     if query_text:
         await update.message.reply_text("사업보고서를 조회하고 있습니다. 잠시만 기다려주세요(30초).")
         (business, companies) = find_company_by_business(update.message.text)
@@ -97,12 +101,16 @@ async def search(update: Update, context: ContextTypes.DEFAULT_TYPE):
         answer = "\n\n".join(answer)
         await update.message.reply_text(f"<i>Query: {business}</i>\n\n{answer}", parse_mode="HTML")
     else:
-        await update.message.reply_text("<code>/search 반도체 장비 회사</code>와 같이 찾으려는 기업이 영위하는 사업을 알려주세요.", parse_mode="HTML")
+        await update.message.reply_text(
+            "<code>/search 반도체 장비 회사</code>와 같이 찾으려는 기업이 영위하는 사업을 알려주세요.",
+            parse_mode="HTML",
+        )
 
 
 if __name__ == "__main__":
     load_dotenv()
 
     app = ApplicationBuilder().token(os.getenv("TELEGRAM_BOT_TOKEN")).build()
+    app.add_handler(CommandHandler("start", start))
     app.add_handler(CommandHandler("search", search))
     app.run_polling()
