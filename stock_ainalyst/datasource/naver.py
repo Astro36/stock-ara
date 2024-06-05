79952a3691757742e519ce86674846e7ed71aef6
Seungjae Park <astro.psj@gmail.com>
2024-06-05 13:24:07 +0900

    Fix bug

--- stock_ainalyst/datasource/naver.py
+++ stock_ainalyst/datasource/naver.py
@@ -9,12 +9,12 @@ def fetch_analysts_comments(symbol: str) -> list[str]:
     r = requests.get(f"https://finance.naver.com/research/company_list.naver?searchType=itemCode&itemCode={symbol}")
 
     comments = []
-    for nid in re.findall(f"company_read.naver\?nid=(\d+)&page=1&searchType=itemCode&itemCode={symbol}", r.text)[:5]:
+    for nid in re.findall(rf"company_read.naver\?nid=(\d+)&page=1&searchType=itemCode&itemCode={symbol}", r.text)[:5]:
         r = requests.get(f"https://finance.naver.com/research/company_read.naver?nid={nid}&page=1&searchType=itemCode&itemCode={symbol}")
         text = r.text.split('class="view_cnt">')[1].split("</td>")[0].strip()
-        text = re.sub("</?[^>]*>", "", text)
-        text = re.sub("[\w]+\.pdf", "", text)
-        text = re.sub("\s+", " ", text)
+        text = re.sub(r"</?[^>]*>", "", text)
+        text = re.sub(r"[\w]+\.pdf", "", text)
+        text = re.sub(r"\s+", " ", text)
         text = text.strip()
         comments.append(text)
     return comments
