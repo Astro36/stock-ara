677dd978394e60cd74c0267364fe9bdca8a245f7
Seungjae Park <astro.psj@gmail.com>
2024-07-07 23:43:29 +0900

    Change portfolio optimization algorithm

--- stock_ara/stock_ara/domain/portfolio_manager.py
+++ stock_ara/stock_ara/domain/portfolio_manager.py
@@ -6,26 +6,34 @@ from stock_ara.infra.database import asset_price_repository, cache_repository
 
 
 class PortfolioManager:
-    def make_optimal_portfolio(self, assets: Asset) -> list[tuple[Asset, float]]:
-        asset_prices = pd.concat([asset_price_repository.get_asset_prices(asset.id) for asset in assets], axis=1)
-        asset_returns = asset_prices.pct_change(fill_method=None).dropna(how="all")
-        asset_covs = asset_returns.cov().fillna(0) * 52
-        asset_expected_returns = pd.Series([cache_repository.get_implied_expected_return(asset.id) for asset in assets], index=[asset.id for asset in assets], dtype=float)
-        asset_expected_returns = asset_expected_returns.to_numpy()
+    def make_optimal_portfolio(self, assets: Asset, risk_free_rate=0.035) -> list[tuple[Asset, float]]:
+        prices = pd.concat([asset_price_repository.get_asset_prices(asset.id) for asset in assets], axis=1)
+        returns = prices.pct_change(fill_method=None).dropna(how="all")
+        covs = returns.cov().fillna(0) * 52
 
-        portfolio_weights = cp.Variable(len(assets))
-        portfolio_return = asset_expected_returns.T @ portfolio_weights
-        portfolio_risk = cp.quad_form(portfolio_weights, asset_covs)
-        prob = cp.Problem(cp.Maximize(portfolio_return - portfolio_risk), [cp.sum(portfolio_weights) == 1, portfolio_weights >= 0])
+        expected_returns = pd.Series([cache_repository.get_implied_expected_return(asset.id) for asset in assets], index=[asset.id for asset in assets], dtype=float).to_numpy()
+        weights = cp.Variable(len(assets))
+        k = cp.Variable()
+
+        prob = cp.Problem(
+            cp.Minimize(cp.quad_form(weights, covs)),
+            [
+                (expected_returns - risk_free_rate).T @ weights == 1,
+                cp.sum(weights) == k,
+                k >= 0,
+            ],
+        )
         prob.solve()
+
+        weights = weights.value / k.value
 
         portfolio = [
             (
                 assets[idx],
                 weight,
                 cache_repository.get_implied_expected_return(assets[idx].id),
-                math.sqrt(asset_covs.iloc[idx, idx]),
+                math.sqrt(covs.iloc[idx, idx]),
             )
-            for idx, weight in enumerate(portfolio_weights.value)
+            for idx, weight in enumerate(weights)
         ]
         return portfolio
